d7372cf1b8eabf3e4583862af404582049187c6e
oarend <oliver.arend@itk-engineering.de>
2018-12-19 09:01:16 +0100

    refactor hide() function, but this is not (yet) reflected in a published .xpi

--- hide.js
+++ hide.js
@@ -19,42 +19,19 @@ const hideSelectors = {
 }
 
 function hide() {
-    let hot = browser.storage.sync.get("hot");
-    let jobs = browser.storage.sync.get("jobs");
-    let meta = browser.storage.sync.get("meta");
-    let newsletter = browser.storage.sync.get("newsletter");
-    let chat = browser.storage.sync.get("chat");
-    let related = browser.storage.sync.get("related");
-    let linked = browser.storage.sync.get("linked");
-    hot.then((res) => { 
-        hideSettings.hot = res.hot ? true : false 
-        jobs.then((res) => { 
-            hideSettings.jobs = res.jobs ? true : false 
-            meta.then((res) => { 
-                hideSettings.meta = res.meta ? true : false 
-                newsletter.then((res) => { 
-                    hideSettings.newsletter = res.newsletter ? true : false
-                    chat.then((res) => { 
-                        hideSettings.chat = res.chat ? true : false
-                        related.then((res) => { 
-                            hideSettings.related = res.related ? true : false
-                            linked.then((res) => { 
-                                hideSettings.linked = res.linked ? true : false
-                                Object.keys(hideSettings).forEach(settingKey => {
-                                    if(hideSettings[settingKey]) {
-                                        hideSelectors[settingKey].forEach(selector => {
-                                            elements = document.querySelectorAll(selector)
-                                            elements.forEach(function(element) {
-                                                element.style.display = "none"
-                                            })
-                                        })
-                                    }
-                                })
-                            })
-                        })
+    let settingKeys = Object.keys(hideSettings)
+    settings = browser.storage.sync.get(settingKeys)
+    settings.then(settings => {
+        settingKeys.forEach(settingKey => { 
+            hideSettings[settingKey] = settings[settingKey] ? true : false
+            if(hideSettings[settingKey]) {
+                hideSelectors[settingKey].forEach(selector => {
+                    elements = document.querySelectorAll(selector)
+                    elements.forEach(function(element) {
+                        element.style.display = "none"
                     })
                 })
-            })
+            }
         })
     })
 }
